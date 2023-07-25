ef5d024571dfec4b594e4f8f5874dc9f54cb4b09
MilvaTech <webmilva@gmail.com>
2023-07-25 14:10:29 -0700

    third commit

--- src/main/java/com/sta/spTest01/SpTest01Application.java
+++ src/main/java/com/sta/spTest01/SpTest01Application.java
@@ -2,23 +2,32 @@ package com.sta.spTest01;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 public class SpTest01Application {
 	private static final Logger log = LoggerFactory.getLogger(SpTest01Application.class);
 	public static void main(String[] args) {
-		log.info("---------------------------------------------");
-		log.info("log in the main class");
-		log.info("---------------------------------------------");
+
 		SpringApplication.run(SpTest01Application.class, args);
 	}
 	@GetMapping("/hello")
 	public String hello(){
 		return "Hello MilvaTech";
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner() throws Exception{
+		return string ->{
+			log.info("---------------------------------------------");
+			log.info("MilvaTech log is active");
+			log.info("---------------------------------------------");
+		};
 	}
 }
