c0e90d718b86d4e9170838e903d7dd0f7b5e167e
MilvaTech <webmilva@gmail.com>
2023-07-25 13:45:17 -0700

    commit second

--- src/main/java/com/sta/spTest01/SpTest01Application.java
+++ src/main/java/com/sta/spTest01/SpTest01Application.java
@@ -1,5 +1,7 @@
 package com.sta.spTest01;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class SpTest01Application {
-
+	private static final Logger log = LoggerFactory.getLogger(SpTest01Application.class);
 	public static void main(String[] args) {
-
+		log.info("---------------------------------------------");
+		log.info("log in the main class");
+		log.info("---------------------------------------------");
 		SpringApplication.run(SpTest01Application.class, args);
 	}
 	@GetMapping("/hello")
